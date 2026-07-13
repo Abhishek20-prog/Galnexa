42a27a84276d44a3dac33475dacc3b4adcb6bc56
Abhishek20-prog <Abhishekkumastudent1234@gmail.com>
2026-07-14 00:22:04 +0530

    commit

--- Galnexa/src/App.jsx
+++ Galnexa/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
           Galnexa
         </h1>
 
-        <p className="font-poppins text-lg text-neutral-500">
+        <p className="font-poppins text-lg text-neutral-600 mt-4 md:text-xl">
           Discover moments worth remembering.
         </p>
       </div>
